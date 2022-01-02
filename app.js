454b654bd58e62707f4763fdbad0a337001843ab
Abdullah-MotiWala <motiwalaabdullah100@gmail.com>
2022-01-02 15:17:52 +0500

    website template done using swiper library

--- app.js
+++ app.js
@@ -11,3 +11,48 @@ const popOpen = () => {
   }
   isOpen = !isOpen;
 };
+
+//swipper
+
+const swiper = new Swiper('.swiper', {
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 5
+    },
+    720:{
+      slidesPerView: 8,
+      spaceBetween: 5
+      
+    }
+  },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+let preLoaderScreen = document.querySelector(".bgScreen");
+const preLoader = () => {
+	preLoaderScreen.style.display = "none"
+};
+document.addEventListener(onload, void(preLoaderScreen.style.display = "none"));
